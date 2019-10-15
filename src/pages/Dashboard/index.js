05d9c68896d0c24d748914fa37b63b9268a90f02
jeison Pedroso <jeison.contas@gmail.com>
2019-10-15 00:51:52 -0300

    falta so news

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,33 +1,37 @@
 import React, { useEffect , useState } from 'react';
-import Api from '../../services/api'
-
+import { Link } from 'react-router-dom'
+import api from '../../services/api'
+import './styles.css'
 
 export default function Dashboard(){
     const [spots,setSpots] = useState([]);
 
     useEffect(()=>{
         async function loadSpots(){
-            const user_id = localStorage.getItem('user')
-            const response = await Api.get('/dashboard',{
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard',{
                 headers: {user_id}
             });
-            setSpots(response.data)
+            setSpots(response.data);
             console.log(response.data)
         }
         loadSpots();
-    },[])
+    },[]);
 
     return (
         <>
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header style={{ backgroundColor : `url(${spot.thumbnail_url})`}}/>
+                        <header style={{ backgroundImage : `url(${spot.thumbnail_url})`}}/>
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
+        <Link to="/new">
+            <button className="btn">Cadastrar novo spot</button>
+        </Link>
         </>
     )
 }
