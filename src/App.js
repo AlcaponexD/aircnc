22239e8a4b77307bf74e891806bf72eeb6415fdc
jeison Pedroso <jeison.contas@gmail.com>
2019-10-14 22:23:36 -0300

    cont aula web quase finalizado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
-import api from './services/api';
+import Routes from './routes';
 
 
 function App() {
-  function handleSubmit(){
-    console.log('hello word')
-  }
 
   return (
       <div className="container">
         <img src={logo} alt="AirCBNC"/>
         <div className="content">
-          <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="email">E-MAIL*</label>
-            <input 
-              id="email" 
-              type="text" 
-              placeholder="Seu melhor email" 
-            />
-            <button type="submit" className="btn">Entrar</button>
-          </form>
+          <Routes></Routes>
+         
         </div>
       </div>
 
